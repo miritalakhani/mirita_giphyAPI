0422a28942bf958bab8f1e3f41a39bbc42dc2c17
Mirita <mirita.lakhani@gmail.com>
2017-05-05 21:50:44 -0400

    updated GIT API call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@
       function displayMovieInfo() {
 
         var movie = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q="+movie+"&api_key=dc6zaTOxFJmzC&limit=10 ";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q="+movie+"&api_key=dc6zaTOxFJmzC&limit=10 ";
 
         // Creating an AJAX call for the specific movie button being clicked
         $.ajax({
